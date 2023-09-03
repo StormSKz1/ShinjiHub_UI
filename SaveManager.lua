b9f327deacdc6668e5290473304ba930b6776059
StormSKz1 <83227365+StormSKz1@users.noreply.github.com>
2023-09-03 11:57:09 +0700

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'Shijin Hub'
+	SaveManager.Folder = 'Shijin'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
